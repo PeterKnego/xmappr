f620221e8229e0b1177c0f735b61bca8a9976717
peter <peter@72705fb0-db51-11de-87ae-b9033455ee04>
2008-12-14 22:47:04 +0000

    delete docx support (moved to separate project)

--- trunk/src/test/java/org/xlite/docx/Footer.java
+++
@@ -1,9 +0,0 @@
-package org.xlite.docx;
-
-/**
- * User: peter
- * Date: Dec 30, 2007
- * Time: 11:08:12 PM
- */
-public class Footer {
-}
--- trunk/src/test/java/org/xlite/docx/Header.java
+++
@@ -1,9 +0,0 @@
-package org.xlite.docx;
-
-/**
- * User: peter
- * Date: Dec 30, 2007
- * Time: 11:08:28 PM
- */
-public class Header {
-}
