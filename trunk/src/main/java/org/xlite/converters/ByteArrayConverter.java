1b5418b15061d40e0c647d11d7992333788a7e3c
peter <peter@72705fb0-db51-11de-87ae-b9033455ee04>
2009-07-04 20:15:02 +0000

    New Base64 codec - XLT-29

--- trunk/src/main/java/org/xlite/converters/ByteArrayConverter.java
+++ trunk/src/main/java/org/xlite/converters/ByteArrayConverter.java
@@ -15,10 +15,10 @@ public class ByteArrayConverter extends ValueConverter {
     }
 
     public Object fromValue(String value, String format) {
-        return Base64.decode(value);
+        return Base64new.decode(value);
     }
 
     public String toValue(Object object, String format) {
-        return String.valueOf(Base64.encode((byte[]) object));
+        return String.valueOf(Base64new.encode((byte[]) object));
     }
 }
