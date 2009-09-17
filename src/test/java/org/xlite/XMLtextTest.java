9fa738cbd9c6e2616856b40fbf26210533592c6b
bostjan <bostjan@72705fb0-db51-11de-87ae-b9033455ee04>
2009-09-17 10:52:09 +0000

    - XLT-39 Remove itemType parameter from @XMLtext annotation

--- src/test/java/org/xlite/XMLtextTest.java
+++ src/test/java/org/xlite/XMLtextTest.java
@@ -6,19 +6,23 @@
  */
 package org.xlite;
 
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.xlite.converters.ValueConverter;
 
-import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class XMLtextTest {
 
-    static final String xml1 = "<one>1</one>";
+    static final String xml1 = "<one>1,2,3</one>";
+    static final String xml2 = "<one>0,1,2,3</one>";
 
 
     @Test
@@ -26,28 +30,25 @@ public class XMLtextTest {
 
         StringReader reader = new StringReader(xml1);
         Configuration conf = new AnnotationConfiguration(XMLTextTest.class, "one");
+        conf.setPrettyPrint(false);
         Xlite xlite = new Xlite(conf);
 
         XMLTextTest one = (XMLTextTest) xlite.fromXML(reader);
-        Assert.assertEquals((int) one.getValues().size(), 2);
+        Assert.assertEquals((int) one.getValues().size(), 4);
         Assert.assertEquals((int) one.getValues().get(0), 0);
         Assert.assertEquals((int) one.getValues().get(1), 1);
+        Assert.assertEquals((int) one.getValues().get(2), 2);
+        Assert.assertEquals((int) one.getValues().get(3), 3);
 
-        try {
-            Writer w = new StringWriter();
-            xlite.toXML(one, w);
-            w.flush();
-            w.close();
-
-            System.out.println(w.toString());
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+        Writer w = new StringWriter();
+        xlite.toXML(one, w);
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLAssert.assertEquals(xml2, w.toString());
     }
 
     public static class XMLTextTest {
 
-        @XMLtext(itemType = Integer.class)
+        @XMLtext(converter = IntegerCollectionConverter.class)
         private List<Integer> values = new ArrayList<Integer>();
 
 
@@ -61,6 +62,32 @@ public class XMLtextTest {
 
         public void setValues(List<Integer> values) {
             this.values = values;
+        }
+    }
+
+    public static class IntegerCollectionConverter extends ValueConverter {
+        public boolean canConvert(Class type) {
+            return Collection.class.isAssignableFrom(type);
+        }
+
+        public Object fromValue(String value, String format, Class targetType) {
+            Collection<Integer> integers = new ArrayList<Integer>();
+            if (value != null) {
+                String[] values = value.split(",");
+                for (String v : values) {
+                    integers.add(Integer.valueOf(v));
+                }
+            }
+            return integers;
+        }
+
+        public String toValue(Object object, String format) {
+            StringBuilder builder = new StringBuilder();
+            Collection<Integer> collection = (Collection<Integer>) object;
+            for (Integer integer : collection) {
+                builder.append(",").append(integer);
+            }
+            return collection.isEmpty() ? null : builder.substring(1);
         }
     }
 
