ab4c7134071297f1910a19917f0c752bc4e76ded
peter@knego.net <peter@knego.net@72705fb0-db51-11de-87ae-b9033455ee04>
2010-02-03 16:36:36 +0000

    Mapping to custom accessor methods (Fixes issue 26)

--- trunk/src/test/java/org/xmappr/InterfaceTest.java
+++ trunk/src/test/java/org/xmappr/InterfaceTest.java
@@ -4,13 +4,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.StringReader;
+import java.lang.reflect.Method;
 
 public class InterfaceTest {
 
     private static String xml = "" +
             "<products>" +
-            "<apple>an apple</apple>" +
-            "<orange>an orange</orange>" +
+            "<apple type='red'>an apple</apple>" +
+            "<orange type='orange'>an orange</orange>" +
             "</products> ";
 
     @Test
@@ -22,7 +23,26 @@ public class InterfaceTest {
 
         Products child = (Products) xmappr.fromXML(reader);
         Assert.assertEquals(child.apple.getText(), "an apple");
+        Assert.assertEquals(child.apple.getType(), "red");
+
         Assert.assertEquals(child.orange.getText(), "an orange");
+        Assert.assertEquals(child.orange.getType(), "orange");
+    }
+
+    public static void main(String[] args) {
+
+        for (Method method : Fruit.class.getMethods()) {
+            Element element = method.getAnnotation(Element.class);
+            System.out.println(element);
+        }
+
+        Orange orange = new Orange();
+        for (Method method : orange.getClass().getMethods()) {
+            System.out.println("method:"+method.getName());
+                System.out.println("annotation:"+ConfigurationProcessor.getTextAnnotation(method).annotationType());
+        }
+
+
     }
 
     @RootElement
@@ -43,24 +63,22 @@ public class InterfaceTest {
 
         @Text
         public String getText();
+
+        @Attribute
+        public void setType(String text);
+
+        @Attribute
+        public String getType();
     }
 
-    public static class Apple extends BaseFruit implements Fruit {
-        public String hiddenText;
+    public static class Apple extends BaseFruit {
 
-
-        public void setText(String text) {
-            hiddenText = text;
-        }
-
-        public String getText() {
-            return hiddenText;
-        }
     }
 
 
     public static class Orange implements Fruit {
         public String hiddenText;
+        public String hiddenType;
 
         public void setText(String text) {
             hiddenText = text;
@@ -69,9 +87,35 @@ public class InterfaceTest {
         public String getText() {
             return hiddenText;
         }
+
+         public void setType(String text) {
+            this.hiddenType = text;
+        }
+
+        public String getType() {
+            return hiddenType;
+        }
     }
 
-    private static class BaseFruit {
-        public String one;
+    private static class BaseFruit implements Fruit{
+        public String hiddenText;
+        public String hiddenType;
+
+
+        public void setText(String text) {
+            hiddenText = text;
+        }
+
+        public String getText() {
+            return hiddenText;
+        }
+
+        public void setType(String text) {
+            this.hiddenType = text;
+        }
+
+        public String getType() {
+            return hiddenType;
+        }
     }
 }
